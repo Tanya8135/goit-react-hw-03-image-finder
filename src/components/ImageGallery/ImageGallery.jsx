45364ae3d082a3f6bd2cdf3ea4f6f3d3962b30e6
tanya8135 <108739694+Tanya8135@users.noreply.github.com>
2023-07-01 10:08:14 +0300

    changed class -> const in ImagesGalery

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,28 +1,23 @@
-import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import ImageGalleryItem from 'components/ImageGalleryItem';
 
 import style from './ImageGallery.module.css';
 
-class ImageGallery extends Component {
-  render() {
-    const { images, onImageClick } = this.props;
-
-    return (
-      <ul className={style.gallery}>
-        {images.map(image => (
-          <ImageGalleryItem
-            key={image.id}
-            imageURL={image.largeImageURL}
-            alt={image.tags}
-            onImageClick={onImageClick}
-          />
-        ))}
-      </ul>
-    );
-  }
-}
+const ImageGallery = ({images, onImageClick}) => {
+  return (
+    <ul className={style.gallery}>
+      {images.map(image => (
+        <ImageGalleryItem
+          key={image.id}
+          imageURL={image.largeImageURL}
+          alt={image.tags}
+          onImageClick={onImageClick}
+        />
+      ))}
+    </ul>
+  );
+};
 
 ImageGallery.propTypes = {
   images: PropTypes.array.isRequired,
